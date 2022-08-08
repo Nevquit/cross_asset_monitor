48c3fd3f72ef64778a3d858b302015e97e70f777
GUO Wei <13911137413@163.com>
2022-08-08 17:05:01 +0800

    'iwanseetting.json'

--- cross_asset_monitor.py
+++ cross_asset_monitor.py
@@ -1,16 +1,18 @@
 from iWAN import iWAN#pip install iWAN
 from monitor_msg_tools import dingMsg,sendEmail,genhtml
+import json
 
-secretkey = '0ea7cb639be0a1c9bf2073f617bee4be66c7c4d1d0e14853ce3a8410d74a31cb'
-Apikey ='1bd59d5aecabc6411a59d5c1daaf3dfde2889627b97d5d5acb05958f4bf9a317'
-url = 'wss://apitest.wanchain.org:8443/ws/v3/'
-url_main = 'wss://api.wanchain.org:8443/ws/v3/'
 
 class CROSS_ASSET_MONITOR:
     def __init__(self,net,):
         self.net = net
         self.url = {'main':'','test':''}
-        self.secretkey = ''
-        self.Apikey = ''
-
-        iwan = iWAN.iWAN()
+        with open('/.iWAN_config','r') as f:
+            config = json.load(f)
+        self.iwan = iWAN.iWAN(config["url_{}".format(net)],config['secretkey'],config['Apikey'])
+        self.dingApi = config["dingApi"]
+        self.address = config["emailAddress"]
+        self.assetblackList = ['EOS']
+        self.toBlackList = []
+        self.chainInfo = requests.get('https://raw.githubusercontent.com/Nevquit/configW/main/chainInfos.json').json()
+        self.poolTokenInfo = requests.get("https://raw.githubusercontent.com/Nevquit/configW/main/crossPoolTokenInfo.json").json()
